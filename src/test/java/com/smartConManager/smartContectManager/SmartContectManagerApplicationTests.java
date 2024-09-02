456d6578112baddf0cbb280d796c9dc77a90f7f3
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-09-02 23:36:37 +0530

    Creating smart Contract manager

---
+++ src/test/java/com/smartConManager/smartContectManager/SmartContectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartConManager.smartContectManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
