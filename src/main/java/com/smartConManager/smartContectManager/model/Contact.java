6af12885589f49d2e21633464a89789a6dcdad2f
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-09-03 00:47:08 +0530

    create Tables

---
+++ src/main/java/com/smartConManager/smartContectManager/model/Contact.java
@@ -0,0 +1,84 @@
+package com.smartConManager.smartContectManager.model;
+
+import jakarta.persistence.*;
+import org.hibernate.Length;
+
+@Entity
+@Table(name="CONTACT")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int cId;
+    private String name;
+    private String nickName;
+    private String work;
+    private String email;
+    private String phone;
+    private String image;
+    @Column(length=50000)
+    private String description;
+
+    public int getcId() {
+        return cId;
+    }
+
+    public void setcId(int cId) {
+        this.cId = cId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
